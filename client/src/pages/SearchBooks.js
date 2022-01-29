ab172ef68e98c4a1dd062228c1d61e8f595b7a9e
Kevin Cortina <kevinc.php@gmail.com>
2022-01-29 13:16:10 -0800

    remove lock package

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -7,7 +7,7 @@ import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
-  const [saveBook, { error , data }] = useMutation(SAVE_BOOK);
+  const [saveBook] = useMutation(SAVE_BOOK);
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
